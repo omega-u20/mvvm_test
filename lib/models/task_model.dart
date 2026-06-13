9c2f8ff87d13195f438c2767435dd4b8706c38ff
kithmini26 <dissanayakekithmini@gmail.com>
2026-06-13 23:18:02 +0530

    Kithmini Added The Model

---
+++ lib/models/task_model.dart
@@ -0,0 +1,16 @@
+class Task {
+  final int id;
+  final String title;
+  final bool isCompleted;
+
+  Task({required this.id, required this.title, required this.isCompleted});
+
+  // Factory method to map Supabase JSON response to our Dart object
+  factory Task.fromJson(Map<String, dynamic> json) {
+    return Task(
+      id: json['id'],
+      title: json['title'],
+      isCompleted: json['is_completed'],
+    );
+  }
+}
